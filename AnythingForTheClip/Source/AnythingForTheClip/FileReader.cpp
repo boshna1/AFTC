f7c34c19b72b3fc4a1bcc4eda5d51d7f335b5a92
boshna1 <joshua.dinata@ontariotechu.net>
2025-11-24 22:12:19 -0500

    added sounds and death

---
+++ AnythingForTheClip/Source/AnythingForTheClip/FileReader.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FileReader.h"
+
+// Sets default values
+AFileReader::AFileReader()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AFileReader::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFileReader::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
